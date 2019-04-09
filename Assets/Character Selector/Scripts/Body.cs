528fe50a55c6f4a9f911fa13206f1c80890b93b9
mfindler <mfindler@cs.wright.edu>
2019-04-09 16:04:58 -0500

    This is a new commit

---
+++ Assets/Character Selector/Scripts/Body.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Math for rand
+
+public class Body : MonoBehaviour {
+
+    public GameObject[] BodyPart;
+    public Material[] material;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        //rend = GetComponent<Renderer>();
+        //rend.enabled = true;
+        //int furColor = Random.value * 3;
+        //rend.sharedMaterial = material[furColor];
+        //int  eyeColor = Random.value * 3;
+        //rend.sharedMaterial = material[furColor];
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
